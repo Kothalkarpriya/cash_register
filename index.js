945b260af77256f4e230c5c8064ed0d75255556a
Priya Kothalkar <priyakothalkar2973@gmail.com>
2021-08-16 17:15:59 +0530

    Created files

---
+++ index.js
@@ -0,0 +1,9 @@
+const billAmount = document.querySelector("#bill-amount");
+
+const cashGiven = document.querySelector("#cash-given");
+
+const checkButton = document.querySelector("#check-button");
+
+checkButton.addEventListener("click", () => console.log("clicked"));
+// console.log(cashGiven.value);
+// billAmount.addEventListener("")
