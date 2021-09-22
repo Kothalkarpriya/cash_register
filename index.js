d7c153d1e05b895f4e25a7dac1acbe99b3e74385
Priya Kothalkar <priyakothalkar2973@gmail.com>
2021-09-22 22:32:32 +0530

    fix: no of notes to be displayed correctly

--- index.js
+++ index.js
@@ -1,36 +1,30 @@
-const billAmount = document.querySelector('#bill-amount');
-const cashGiven = document.querySelector('#cash-given');
-const button = document.querySelector('#btn');
+var billAmount = document.querySelector('#bill-amount');
+var cashGiven = document.querySelector('#cash-given');
+var button = document.querySelector('#btn');
+var output = document.querySelector('#output');
+var message = document.querySelector('#error-msg');
+var noOfNote = document.querySelectorAll(".notes");
 
-const message = document.querySelector('#error-msg');
-const noOfNote = document.querySelectorAll(".notes");
-
-// const hideButton = document.querySelector("#hiding");
-
-//Hiding from the button
-// hideButton.addEventListener('click', function(){
-//     cashGiven.style.visibility = "hidden";
-// })
-
-const availableNote = [2000,500,100,50,20,10,5,1];
+const availableNote = [2000,500,100,20,10,5,1];
 button.addEventListener('click', function validateFunction(){
-    msgHide();
     // First checking wherther the bill is grater than zero
-    if(billAmount.value > 0){
+    var cashgiven = Number(cashGiven.value);
+    var billamount = Number(billAmount.value);
+    if(cashgiven > 0 && billamount > 0){
         // Checking bill is smaller than our cash or not
         // If it is small then we can pay the bill.
-        if(cashGiven.value >= billAmount.value){
-            const returnAmount = cashGiven.value - billAmount.value;
+        if(cashgiven >= billamount){
+            const returnAmount = cashgiven - billamount;
             calculateFun(returnAmount);
 
         }else{
             //bill amount is greater than cash
-            console.log('wront input');
-            errorMsgFun('Invalid Input');
+            // console.log('wront input');
+            errorMsgFun('Bill amount is greater than Cash amount');
         }
     }else{
         // bill is less  than small
-        errorMsgFun('Invalid Input');
+        errorMsgFun('Do you want to wash plates?');
     }
 });
 // Calculate function: 
@@ -49,8 +43,11 @@ button.addEventListener('click', function validateFunction(){
 //Math.trunc(10 /10 )= 1
 //10 % 10 => 0
 function calculateFun(returnAmount){
-
     console.log('entered in to calculate');
+    output.innerText = 'Amount to be returned' +parseInt(returnAmount);
+    if(returnAmount < 1 ){
+        errorMsgFun('No amount to be returned');
+    }
     for(let i = 0 ; i < availableNote.length; i++){
         const numberOfNodes = Math.trunc(
             returnAmount / availableNote[i]
@@ -59,10 +56,20 @@ function calculateFun(returnAmount){
         noOfNote[i].innerText = numberOfNodes;
     }
 }
+// function compute(remainder, noteAmt, index){
+//     let mod = remainder;
+//     if(mod >= noteAmt){
+//         mod = remainder % noteAmt;
+//         remainder = remainder - mod;
+//         let notes = remainder / noteAmt;
+//         noOfNote[index].innerText = notes;
+//     }
+//     return mod;
+// }
 
-function msgHide(){
-    message.style.display = "none";
-}
+// function msgHide(){
+//     message.style.display = "none";
+// }
 
 function errorMsgFun(msg){
     message.style.display = 'block';
