65f2bc9d5af9f06ad23ab1d5a2ebeddd0894e61d
Priya Kothalkar <priyakothalkar2973@gmail.com>
2021-09-23 18:28:00 +0530

    fix: error handling and styling

--- index.js
+++ index.js
@@ -5,26 +5,27 @@ var output = document.querySelector('#output');
 var message = document.querySelector('#error-msg');
 var noOfNote = document.querySelectorAll(".notes");
 
-const availableNote = [2000,500,100,50,20,10,5,1];
-button.addEventListener('click', function validateFunction(){
+const availableNote = [2000, 500, 100, 50, 20, 10, 5, 1];
+button.addEventListener('click', function validateFunction() {
     // First checking wherther the bill is grater than zero
+    msgHide();
     var cashgiven = Number(cashGiven.value);
     var billamount = Number(billAmount.value);
-    if(cashgiven > 0 && billamount > 0){
+    if (cashgiven > 0 && billamount > 0) {
         // Checking bill is smaller than our cash or not
         // If it is small then we can pay the bill.
-        if(cashgiven >= billamount){
+        if (cashgiven >= billamount) {
             const returnAmount = cashgiven - billamount;
             calculateFun(returnAmount);
 
-        }else{
+        } else {
             //bill amount is greater than cash
             // console.log('wront input');
-            errorMsgFun('Bill amount is greater than Cash amount');
+            errorMsgFun('Cash should be greater than or equals to bill');
         }
-    }else{
-        // bill is less  than small
-        errorMsgFun('Do you want to wash plates?');
+    }else {
+    // bill is less  than small
+    errorMsgFun('Do you want to wash plates?');
     }
 });
 // Calculate function: 
@@ -42,13 +43,13 @@ button.addEventListener('click', function validateFunction(){
 // 50 % 20 => 10
 //Math.trunc(10 /10 )= 1
 //10 % 10 => 0
-function calculateFun(returnAmount){
-    console.log('entered in to calculate');
-    output.innerText = 'Amount to be returned' +parseInt(returnAmount);
-    if(returnAmount < 1 ){
+function calculateFun(returnAmount) {
+    // console.log('entered in to calculate');
+    output.innerText = 'Amount to be returned: ' + parseInt(returnAmount);
+    if (returnAmount < 1) {
         errorMsgFun('No amount to be returned');
     }
-    for(let i = 0 ; i < availableNote.length; i++){
+    for (let i = 0; i < availableNote.length; i++) {
         const numberOfNodes = Math.trunc(
             returnAmount / availableNote[i]
         );
@@ -56,22 +57,13 @@ function calculateFun(returnAmount){
         noOfNote[i].innerText = numberOfNodes;
     }
 }
-// function compute(remainder, noteAmt, index){
-//     let mod = remainder;
-//     if(mod >= noteAmt){
-//         mod = remainder % noteAmt;
-//         remainder = remainder - mod;
-//         let notes = remainder / noteAmt;
-//         noOfNote[index].innerText = notes;
-//     }
-//     return mod;
-// }
 
-// function msgHide(){
-//     message.style.display = "none";
-// }
 
-function errorMsgFun(msg){
+function msgHide(){
+    message.style.display = "none";
+}
+
+function errorMsgFun(msg) {
     message.style.display = 'block';
     message.innerText = msg;
 }
